874e4de1caaa04f68064bf9e8a1933132f2e5158
Aya BIBET <aya.bibet@ensia.edu.dz>
2026-02-07 21:20:12 +0200

    Delete newtestlostitems directory

--- newtestlostitems/api/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: application/json");
-echo json_encode([
-    "status" => "ok",
-    "message" => "API is working",
-    "method" => $_SERVER['REQUEST_METHOD'],
-    "php_version" => phpversion()
-]);
-?>
